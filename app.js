1a012df8a124e9781107d00c30b3002ca9fcbf24
Vigneshvicky9787 <vicky5760rr@gmail.com>
2021-12-20 21:35:50 +0530

    console comments

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log('my first js code...')
+console.log(true)
+console.log('String',{name: "vicky"})
+console.warn('this is a warning')
+console.error('404 error')
+console.log( {name: 'vicky'})
+
+let arr =[0,1 , 2,]
+arr.push('value')
+console.log(arr)
+for( var i=2; i<9; i++){
+console.log(i)
+}
+var j = 18 ;
+while(j<23)
+{
+    console.log(j)
+    j++;
+    
+}
+var no = [1 , 3, 2, 9];
+no.forEach(function(no1){
+  console.log(no1)
+}
+);
+var a = 1;
+var b =1;
+var c = 3;
+
+if (a == b && c==3){
+ console.log("true")
+}
+
+else{
+ console.log('false')
+}
