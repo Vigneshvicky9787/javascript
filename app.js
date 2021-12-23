9fbb97be95b1429fc1d99d462231ef40f64ee50f
Vigneshvicky9787 <vicky5760rr@gmail.com>
2021-12-23 16:02:05 +0530

    funtion keywords

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
-// const element = document.getElementById('selectme')
- 
-//  element.classList.add('five')
+const punch = document.querySelector('#punch')
 
-//  console.log(element.classList)
-//  element.classList.remove('one')
-//  const values = element.classList.contains('five')
+console.log(punch)
 
-// console.log(values)
+const scissor = document.querySelector('#scissor')
+const paper = document.querySelector('#paper')
 
-// element.innerHTML = 'This is js'
 
-// element.style.color = 'blue';
-// element.style.background = 'skyblue';
-// element.style.fontSize = '24px';
-
-// element.style = 'display: none'
-// // const print = document.getElementsByClassName('heading')
-
-const print = document.getElementsByTagName('h2')
-
-console.log(print)
-
-const seletor = document.querySelector('h4.heading')
-
-console.log(seletor)
-//----------------------------------------------------------
-
-const button = document.querySelector('button')
-
-const dark = document.querySelector('body')
-
-dark.addEventListener('click' , ()=>{
-  dark.classList.toggle('aqua')
+punch.addEventListener('click',()=>{
+    console.log('you clicked punch')
 })
 
+scissor.addEventListener('click',()=>{
+    console.log('you clicked scissor')
+})
+
+paper.addEventListener('click',()=>{
+    console.log('you clicked paper')
+})
+
+// ---------------------------------------------------------
+
+function add (a=10, b=0){
+    return  a+b
+}
+// const result = add(23,7)
+console.log(add(39,1))
