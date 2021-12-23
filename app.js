f440b949485bc16202c93b2b6e079c7d8549e3d9
Vigneshvicky9787 <vicky5760rr@gmail.com>
2021-12-23 10:58:27 +0530

    dom basic elements

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const element = document.getElementById('selectme')
+// const element = document.getElementById('selectme')
+ 
+//  element.classList.add('five')
 
-console.log( element )
+//  console.log(element.classList)
+//  element.classList.remove('one')
+//  const values = element.classList.contains('five')
 
-// const print = document.getElementsByClassName('heading')
+// console.log(values)
+
+// element.innerHTML = 'This is js'
+
+// element.style.color = 'blue';
+// element.style.background = 'skyblue';
+// element.style.fontSize = '24px';
+
+// element.style = 'display: none'
+// // const print = document.getElementsByClassName('heading')
 
 const print = document.getElementsByTagName('h2')
 
@@ -11,3 +24,13 @@ console.log(print)
 const seletor = document.querySelector('h4.heading')
 
 console.log(seletor)
+//----------------------------------------------------------
+
+const button = document.querySelector('button')
+
+const dark = document.querySelector('body')
+
+dark.addEventListener('click' , ()=>{
+  dark.classList.toggle('aqua')
+})
+
